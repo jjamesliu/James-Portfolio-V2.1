7e6ef809433a429e3c44ada6c2e2ae2cb9753926
jjamesliu <liujameszhi@gmail.com>
2025-07-12 16:54:03 -0700

    netlify changes

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ import path from "path"
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss(),],
-  base: 'James-Portfolio-V2',
+  base: './',
   resolve: {
   alias: {
       "@": path.resolve(__dirname, "./src"),
