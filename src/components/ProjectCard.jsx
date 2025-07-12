b71acc7eed13d97694d7ed9e52a74077781feb96
jjamesliu <liujameszhi@gmail.com>
2025-07-12 15:20:43 -0700

    button added but need to add functionality

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,4 +1,8 @@
 import'./ProjectCard.css'
+import { Button } from '@/components/ui/button'
+import { Github, Globe } from 'lucide-react';
+
+
 
 function ProjectCard(props) {
  return (
@@ -11,11 +15,19 @@ function ProjectCard(props) {
                     <img src={props.img}/>
                 </div>
                 <div>
+
+
                 <a className="projectTitle" href={props.link} target="_blank">{props.title}</a>
+
                 <p>
                     {props.description}
                 </p>
                 </div>
+                <Button variant="default" 
+                className='!mr-[0.5rem] !mt-[0.4rem] !px-[2rem] gap-1'>
+                <Github />GitHub</Button>
+                <Button variant="default"
+                className='!mr-[0.5rem] !mt-[0.4rem] !px-[2rem] gap-1'> <Globe />Website</Button>
             </div>
 
         </div>
